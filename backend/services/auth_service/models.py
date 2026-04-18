e1357f129276f35b484ccfbcb40b79e895b43fd6
Bounab Chaima <103075196+chaimabouneb@users.noreply.github.com>
2026-04-18 22:21:12 +0100

    Add is_active field to user model

--- backend/services/auth_service/models.py
+++ backend/services/auth_service/models.py
@@ -56,7 +56,7 @@ class Utilisateur(AbstractBaseUser):
     password = models.CharField(max_length=255, db_column="password_hash")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-
+    is_active = models.BooleanField(default=True)
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
 
